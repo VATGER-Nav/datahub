e21220fc372b8b3b18e2c6ee8e18dd4ed207a436
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2023-10-24 14:22:03 +0200

    add schedule json creation workflow

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -1,6 +1,7 @@
 from formatJsons import format_and_sort_json_files
 from consolidateData import consolidateJsons
 from createTsBotJson import createTeamspeakStationBotJson
+from createScheduleJson import createScheduleJson
 
 combinedFile = "data.json"
 folders_to_sort = ['edgg', 'edmm', 'eduu', 'edww', 'edyy', 'event_schedules']
@@ -11,6 +12,9 @@ format_and_sort_json_files(folders_to_sort)
 
 # consolidate all source files into one combined file
 consolidateJsons(folders_to_consolidate, combinedFile)
+
+# create schedule json
+createScheduleJson(combinedFile, "legacy/schedule.json")
 
 # create legacy jsons
 createTeamspeakStationBotJson(combinedFile, "legacy/atc_station_mappings.json")
