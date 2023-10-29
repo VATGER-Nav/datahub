65e8178bd6bb506e324aa2b5907a63391f142519
LeoKle <leon.kleinschmidt@outlook.de>
2023-10-29 12:22:13 +0100

    add status report
    required for status checks

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -1,25 +1,29 @@
+import sys
 from formatJsons import format_and_sort_json_files
 from consolidateData import consolidateJsons
 from createTsBotJson import createTeamspeakStationBotJson
 from createScheduleJson import createScheduleJson
 from removeRedundant import removeRedundantScheduleEntries
 
-combinedFile = "data.json"
-folders_to_sort = ['edgg', 'edmm', 'eduu', 'edww', 'edyy', 'event_schedules']
-folders_to_consolidate = ['edgg', 'edmm', 'eduu', 'edww', 'edyy']
+try:
+    combinedFile = "data.json"
+    folders_to_sort = ['edgg', 'edmm', 'eduu', 'edww', 'edyy', 'event_schedules']
+    folders_to_consolidate = ['edgg', 'edmm', 'eduu', 'edww', 'edyy']
 
-# remove redundant schedule_groups entries
-removeRedundantScheduleEntries(folders_to_consolidate)
+    # remove redundant schedule_groups entries
+    removeRedundantScheduleEntries(folders_to_consolidate)
 
-# format all source jsons
-format_and_sort_json_files(folders_to_sort)
+    # format all source jsons
+    format_and_sort_json_files(folders_to_sort)
 
-# consolidate all source files into one combined file
-consolidateJsons(folders_to_consolidate, combinedFile)
+    # consolidate all source files into one combined file
+    consolidateJsons(folders_to_consolidate, combinedFile)
 
-# create schedule json
-createScheduleJson(combinedFile, "legacy/schedule.json")
+    # create schedule json
+    createScheduleJson(combinedFile, "legacy/schedule.json")
 
-# create legacy jsons
-createTeamspeakStationBotJson(combinedFile, "legacy/atc_station_mappings.json")
+    # create legacy jsons
+    createTeamspeakStationBotJson(combinedFile, "legacy/atc_station_mappings.json")
 
+except Exception as error:
+    print(error)
